18d82bb2dbc7975cb529336363474430c7b69a1d
Kamin4ri <kaminari.k2@gmail.com>
2016-01-09 00:00:02 -0300

    Update build scripts

--- build.sh
+++ build.sh
@@ -2,10 +2,7 @@
 
 # Variables
 device="$1";
-version="$2";
 zipname="";
-zipdir="zip_"$device;
-outdir="release_"$device;
 
 # Set up the cross-compiler
 export PATH=$HOME/Toolchains/Linaro-5.2-A7/bin:$PATH;
@@ -25,15 +22,25 @@ else
 	echo -e "Invalid device. Aborting.";
 	exit 1;
 fi;
-if [ $version ]; then
-	echo -e "Version: "$version"\n";
+if [ $2 ]; then
+	if [ ! $2 == "clean" ]; then
+		version="$2";		
+		echo -e "Version: "$version"\n";
+	else
+		echo -e "No version number has been set. The build date & time will be used instead.\n";
+	fi;
 else
 	echo -e "No version number has been set. The build date & time will be used instead.\n";
 fi;
 
-# Clear the result of previous builds if $3 == clean
+# Clear the result of previous builds if $2 (or $3) == clean
 if [ $3 ]; then
 	if [ $3 == "clean" ]; then
+		echo -e "The output of previous builds will be removed.\n";
+		make clean && make mrproper;
+	fi;
+elif [ $2 ]; then
+	if [ $2 == "clean" ]; then
 		echo -e "The output of previous builds will be removed.\n";
 		make clean && make mrproper;
 	fi;
@@ -43,22 +50,31 @@ fi;
 make kaminari/"$device"_defconfig;
 if [ $4 ]; then
 	make -j$4;
+elif [ $3 ]; then
+	make -j$3;
 else
 	make -j3;
 fi;
 
 # Set the build date & time after it has been completed
 builddate=`date +%Y%m%d.%H%M%S`;
+builddate_full=`date +"%d %b %Y | %H:%M:%S %Z"`;
 
-# Make dirs if they don't exist
-if [ ! -d ../$zipdir ]; then mkdir ../$zipdir; fi;
-if [ ! -d ../$zipdir/modules ]; then mkdir ../$zipdir/modules; fi;
+zipdir="zip_"$device;
+outdir="release_"$device;
+
+# Clone the git repo if the zip dir doesn't exist
+if [ ! -d ../$zipdir ]; then
+	git clone -b $device https://github.com/Kamin4ri/Custom_Anykernel ../$zipdir;
+fi;
+
+# Make the release dir if it doesn't exist
 if [ ! -d ../$outdir ]; then mkdir ../$outdir; fi;
 
 # Remove previous modules
 if [ -d ../$zipdir/modules ]; then rm -rf ../$zipdir/modules/*; fi;
 
-# Make dirs part 2
+# Make wi-fi module dir
 if [ ! -d ../$zipdir/modules/pronto ]; then mkdir ../$zipdir/modules/pronto; fi;
 
 # Modules
@@ -82,6 +98,9 @@ case $version in
 esac;
 
 # Make the zip
-echo $version > ../$zipdir/version.txt;
+if [ $version ]; then
+	echo "Version: $version" > version.txt;
+else
+	echo "Build date and time: $builddate_full" > version.txt;
 zip -r9 $zipname.zip * > /dev/null;
 mv $zipname.zip ../$outdir;
