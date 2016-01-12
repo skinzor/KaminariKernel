cb47c04c7f9ae46959bac40a31cfc27ecc496351
Kamin4ri <kaminari.k2@gmail.com>
2016-01-12 12:18:24 -0300

    Revert "drivers: notifier: add state notifier driver"
    This reverts commit d0895cd1311a44e0ce2f722c94cb0047b982f069.
    
    Signed-off-by: Kamin4ri <kaminari.k2@gmail.com>

--- drivers/video/msm/mdss/mdss_dsi.c
+++ drivers/video/msm/mdss/mdss_dsi.c
@@ -24,10 +24,6 @@
 #include <linux/regulator/consumer.h>
 #include <linux/lcd_notify.h>
 
-#ifdef CONFIG_STATE_NOTIFIER
-#include <linux/state_notifier.h>
-#endif
-
 #include "mdss.h"
 #include "mdss_mdp.h"
 #include "mdss_panel.h"
@@ -1028,11 +1024,9 @@ static int mdss_dsi_event_handler(struct mdss_panel_data *pdata,
 		if (ctrl_pdata->on_cmds.link_state == DSI_HS_MODE)
 			rc = mdss_dsi_unblank(pdata);
 		pdata->panel_info.cont_splash_esd_rdy = true;
-#ifdef CONFIG_STATE_NOTIFIER
-		if (!use_fb_notifier)
-			state_notifier_call_chain(STATE_NOTIFIER_ACTIVE, NULL);
-#endif
+
 		lcd_notifier_call_chain(LCD_EVENT_ON_END, NULL);
+
 		break;
 	case MDSS_EVENT_BLANK:
 		lcd_notifier_call_chain(LCD_EVENT_OFF_START, NULL);
@@ -1046,10 +1040,6 @@ static int mdss_dsi_event_handler(struct mdss_panel_data *pdata,
 		rc = mdss_dsi_off(pdata);
 		lcd_notifier_call_chain(LCD_EVENT_OFF_END, NULL);
 		break;
-#ifdef CONFIG_STATE_NOTIFIER
-		if (!use_fb_notifier)
-			state_notifier_call_chain(STATE_NOTIFIER_SUSPEND, NULL);
-#endif
 	case MDSS_EVENT_CONT_SPLASH_FINISH:
 		if (ctrl_pdata->off_cmds.link_state == DSI_LP_MODE)
 			rc = mdss_dsi_blank(pdata);
