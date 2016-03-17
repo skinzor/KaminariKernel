cfd87a151bfb64af3e1752a78fc8ca1e903aa2e0
Kamin4ri <kamin4ridev@gmail.com>
2016-03-17 01:56:46 -0300

    Revert "msm: mdss: Adding lcd notifier"
    This reverts commit 2c5eed65281cd14d3105e84470f61eff53c8c35b.
    
    Signed-off-by: Kamin4ri <kamin4ridev@gmail.com>

--- drivers/video/msm/mdss/mdss_dsi.c
+++ drivers/video/msm/mdss/mdss_dsi.c
@@ -23,10 +23,6 @@
 #include <linux/uaccess.h>
 #include <linux/regulator/consumer.h>
 
-#ifdef CONFIG_LCD_NOTIFY
-#include <linux/lcd_notify.h>
-#endif
-
 #ifdef CONFIG_STATE_NOTIFIER
 #include <linux/state_notifier.h>
 #endif
@@ -1019,9 +1015,6 @@ static int mdss_dsi_event_handler(struct mdss_panel_data *pdata,
 
 	switch (event) {
 	case MDSS_EVENT_UNBLANK:
-#ifdef CONFIG_LCD_NOTIFY
-		lcd_notifier_call_chain(LCD_EVENT_ON_START, NULL);
-#endif
 		rc = mdss_dsi_on(pdata);
 		mdss_dsi_op_mode_config(pdata->panel_info.mipi.mode,
 							pdata);
@@ -1036,14 +1029,8 @@ static int mdss_dsi_event_handler(struct mdss_panel_data *pdata,
 #ifdef CONFIG_STATE_NOTIFIER
 		state_notifier_call_chain(STATE_NOTIFIER_ACTIVE, NULL);
 #endif
-#ifdef CONFIG_LCD_NOTIFY
-		lcd_notifier_call_chain(LCD_EVENT_ON_END, NULL);
-#endif
 		break;
 	case MDSS_EVENT_BLANK:
-#ifdef CONFIG_LCD_NOTIFY
-		lcd_notifier_call_chain(LCD_EVENT_OFF_START, NULL);
-#endif
 		if (ctrl_pdata->off_cmds.link_state == DSI_HS_MODE)
 			rc = mdss_dsi_blank(pdata);
 		break;
@@ -1052,9 +1039,6 @@ static int mdss_dsi_event_handler(struct mdss_panel_data *pdata,
 		if (ctrl_pdata->off_cmds.link_state == DSI_LP_MODE)
 			rc = mdss_dsi_blank(pdata);
 		rc = mdss_dsi_off(pdata);
-#ifdef CONFIG_LCD_NOTIFY
-		lcd_notifier_call_chain(LCD_EVENT_OFF_END, NULL);
-#endif
 		break;
 #ifdef CONFIG_STATE_NOTIFIER
 		state_notifier_call_chain(STATE_NOTIFIER_SUSPEND, NULL);
