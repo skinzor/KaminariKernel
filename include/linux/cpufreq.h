be5e370cf563f6012e97a0bddb42751e6bb50c1f
Kamin4ri <kamin4ridev@gmail.com>
2016-06-02 11:04:19 -0300

    drivers/cpufreq: Add zzmoove governor
    Signed-off-by: Kamin4ri <kamin4ridev@gmail.com>

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -383,6 +383,9 @@ extern struct cpufreq_governor cpufreq_gov_interactive;
 extern struct cpufreq_governor cpufreq_gov_conservative;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_conservative)
 // Additional governors
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_ZZMOOVE)
+extern struct cpufreq_governor cpufreq_gov_zzmoove;
+#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_zzmoove)
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_YANKDEMAND)
 extern struct cpufreq_governor cpufreq_gov_yankdemand;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_yankdemand)
