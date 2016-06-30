5a02e0c4fa6f66363bedad1fa8cb078b50361f6d
Kamin4ri <kamin4ridev@gmail.com>
2016-06-30 12:45:59 -0300

    Initial build scripts
    Signed-off-by: Kamin4ri <kamin4ridev@gmail.com>

---
+++ falconbuild.sh
@@ -0,0 +1,127 @@
+#!/bin/bash
+
+# Variables
+sequence=`seq 1 100`;
+numjobs=0;
+this="KaminariKernel";
+
+# Set up the cross-compiler
+export PATH=$HOME/Toolchains/Linaro-5.3-Generic/bin:$PATH;
+export ARCH=arm;
+export SUBARCH=arm;
+export CROSS_COMPILE=arm-linux-gnueabihf-;
+
+# Clear the screen, bud!
+clear;
+
+# Output some basic info
+echo -e "Building KaminariKernel...";
+echo -e "Device: Moto G (falcon)\n";
+
+while read -p "Do you wanna clean everything (generated files, etc.)? (Y/N) " clean; do
+	case $clean in
+		"y" | "Y" | "yes" | "Yes")
+			echo -e "Cleaning everything...\n";
+			make --quiet mrproper && echo -e "Done!\n";
+			break;;
+		"n" | "N" | "no" | "No" | "" | " ")
+			echo -e "Not cleaning anything.\n";
+			break;;
+		*)
+			echo -e "\nInvalid option. Try again.\n";;
+	esac;
+done;
+
+while read -p "Do you wanna specify a release/version number? (Just press enter if you don't.) " rel; do
+	if [[ `echo $rel | gawk --re-interval "/^R/"` != "" ]]; then
+		for i in $sequence; do
+			if [ `echo $rel | gawk --re-interval "/$i/"` ]; then
+				echo -e "Release number: $rel\n";
+				export LOCALVERSION="-Kaminari-$rel";
+				version=$rel;
+			fi;
+		done;
+	elif [[ `echo $rel | gawk --re-interval "/^v/"` ]]; then
+		echo -e "Version number: $rel\n";
+		export LOCALVERSION="-Kaminari-$rel";
+		version=$rel;
+	else
+		case $rel in
+			"" | " " )
+				echo -e "No release number specified. Assuming test/nightly build.\n";
+				export LOCALVERSION="-Kaminari-Testing";
+				version=`date "+%Y%m%d.%H%M%S"`;
+				break;;
+			*)
+				break;;
+		esac;
+	fi;
+	break;
+done;
+
+while read -p "How many parallel jobs do you wanna use? (Default is 4.) " numjobs; do
+	for i in $sequence; do
+		if [[ $numjobs = $i ]]; then
+			echo -e "Number of custom jobs: $numjobs\n";
+			jobs=$numjobs;
+		else
+			case $numjobs in
+				"" | " ")
+					echo -e "No custom number of jobs specified. Using default number.\n";
+					jobs="4";
+					break;;
+				*)
+					echo -e "\nInvalid option. Try again.\n";;
+			esac;
+		fi;
+	done;
+	break;
+done;
+	
+echo -e "Build started on: `date +"%A, %d %B %Y @ %H:%M:%S %Z (GMT %:z)"`";
+			
+# Build the kernel
+make falcon_defconfig;
+
+if [ "$jobs" != "0" ]; then
+	make -j$jobs;
+else
+	make;
+fi;
+
+# Tell when the build was finished
+echo -e "Build finished on: `date +"%A, %d %B %Y @ %H:%M:%S %Z (GMT %:z)"`\n";
+
+# Make the zip dir if it doesn't exist
+if [ ! -d ../Zip_CustomMM_Falcon ]; then 
+	mkdir ../Zip_CustomMM_Falcon;
+fi;
+
+# Make the modules dir if it doesn't exist
+if [ ! -d ../Zip_CustomMM_Falcon/modules ]; then mkdir ../Zip_CustomMM_Falcon/modules; fi;
+
+# Make the release dir if it doesn't exist
+if [ ! -d ../Out_CustomMM_Falcon ]; then mkdir ../Out_CustomMM_Falcon; fi;
+
+# Remove previous modules
+if [ -d ../Zip_CustomMM_Falcon/modules ]; then rm -rf ../Zip_CustomMM_Falcon/modules/*; fi;
+
+# Make wi-fi module dir
+if [ ! -d ../Zip_CustomMM_Falcon/modules/pronto ]; then mkdir ../Zip_CustomMM_Falcon/modules/pronto; fi;
+
+# Modules
+find ./ -type f -name '*.ko' -exec cp -f {} ../Zip_CustomMM_Falcon/modules/ \;
+mv ../Zip_CustomMM_Falcon/modules/wlan.ko ../Zip_CustomMM_Falcon/modules/pronto/pronto_wlan.ko;
+
+# Copy zImage-dtb
+cp -f arch/arm/boot/zImage-dtb ../Zip_CustomMM_Falcon/;
+ls -l ../Zip_CustomMM_Falcon/zImage-dtb;
+cd ../Zip_CustomMM_Falcon;
+
+# Set zip name
+zipname="Kaminari_"$version"_Falcon";
+
+# Make the zip
+echo -e "Version: $version" > version.txt;
+zip -r9 $zipname.zip * > /dev/null;
+mv $zipname.zip ../Out_CustomMM_Falcon;
---
+++ peregrinebuild.sh
@@ -0,0 +1,127 @@
+#!/bin/bash
+
+# Variables
+sequence=`seq 1 100`;
+numjobs=0;
+this="KaminariKernel";
+
+# Set up the cross-compiler
+export PATH=$HOME/Toolchains/Linaro-5.3-Generic/bin:$PATH;
+export ARCH=arm;
+export SUBARCH=arm;
+export CROSS_COMPILE=arm-linux-gnueabihf-;
+
+# Clear the screen, bud!
+clear;
+
+# Output some basic info
+echo -e "Building KaminariKernel...";
+echo -e "Device: Moto G 4G (peregrine)\n";
+
+while read -p "Do you wanna clean everything (generated files, etc.)? (Y/N) " clean; do
+	case $clean in
+		"y" | "Y" | "yes" | "Yes")
+			echo -e "Cleaning everything...\n";
+			make --quiet mrproper && echo -e "Done!\n";
+			break;;
+		"n" | "N" | "no" | "No" | "" | " ")
+			echo -e "Not cleaning anything.\n";
+			break;;
+		*)
+			echo -e "\nInvalid option. Try again.\n";;
+	esac;
+done;
+
+while read -p "Do you wanna specify a release/version number? (Just press enter if you don't.) " rel; do
+	if [[ `echo $rel | gawk --re-interval "/^R/"` != "" ]]; then
+		for i in $sequence; do
+			if [ `echo $rel | gawk --re-interval "/$i/"` ]; then
+				echo -e "Release number: $rel\n";
+				export LOCALVERSION="-Kaminari-$rel";
+				version=$rel;
+			fi;
+		done;
+	elif [[ `echo $rel | gawk --re-interval "/^v/"` ]]; then
+		echo -e "Version number: $rel\n";
+		export LOCALVERSION="-Kaminari-$rel";
+		version=$rel;
+	else
+		case $rel in
+			"" | " " )
+				echo -e "No release number specified. Assuming test/nightly build.\n";
+				export LOCALVERSION="-Kaminari-Testing";
+				version=`date "+%Y%m%d.%H%M%S"`;
+				break;;
+			*)
+				break;;
+		esac;
+	fi;
+	break;
+done;
+
+while read -p "How many parallel jobs do you wanna use? (Default is 4.) " numjobs; do
+	for i in $sequence; do
+		if [[ $numjobs = $i ]]; then
+			echo -e "Number of custom jobs: $numjobs\n";
+			jobs=$numjobs;
+		else
+			case $numjobs in
+				"" | " ")
+					echo -e "No custom number of jobs specified. Using default number.\n";
+					jobs="4";
+					break;;
+				*)
+					echo -e "\nInvalid option. Try again.\n";;
+			esac;
+		fi;
+	done;
+	break;
+done;
+	
+echo -e "Build started on: `date +"%A, %d %B %Y @ %H:%M:%S %Z (GMT %:z)"`";
+			
+# Build the kernel
+make peregrine_defconfig;
+
+if [ "$jobs" != "0" ]; then
+	make -j$jobs;
+else
+	make;
+fi;
+
+# Tell when the build was finished
+echo -e "Build finished on: `date +"%A, %d %B %Y @ %H:%M:%S %Z (GMT %:z)"`\n";
+
+# Make the zip dir if it doesn't exist
+if [ ! -d ../Zip_CustomMM_Peregrine ]; then 
+	mkdir ../Zip_CustomMM_Peregrine;
+fi;
+
+# Make the modules dir if it doesn't exist
+if [ ! -d ../Zip_CustomMM_Peregrine/modules ]; then mkdir ../Zip_CustomMM_Peregrine/modules; fi;
+
+# Make the release dir if it doesn't exist
+if [ ! -d ../Out_CustomMM_Peregrine ]; then mkdir ../Out_CustomMM_Peregrine; fi;
+
+# Remove previous modules
+if [ -d ../Zip_CustomMM_Peregrine/modules ]; then rm -rf ../Zip_CustomMM_Peregrine/modules/*; fi;
+
+# Make wi-fi module dir
+if [ ! -d ../Zip_CustomMM_Peregrine/modules/pronto ]; then mkdir ../Zip_CustomMM_Peregrine/modules/pronto; fi;
+
+# Modules
+find ./ -type f -name '*.ko' -exec cp -f {} ../Zip_CustomMM_Peregrine/modules/ \;
+mv ../Zip_CustomMM_Peregrine/modules/wlan.ko ../Zip_CustomMM_Peregrine/modules/pronto/pronto_wlan.ko;
+
+# Copy zImage-dtb
+cp -f arch/arm/boot/zImage-dtb ../Zip_CustomMM_Peregrine/;
+ls -l ../Zip_CustomMM_Peregrine/zImage-dtb;
+cd ../Zip_CustomMM_Peregrine;
+
+# Set zip name
+zipname="Kaminari_"$version"_Peregrine";
+
+# Make the zip
+echo -e "Version: $version" > version.txt;
+zip -r9 $zipname.zip * > /dev/null;
+mv $zipname.zip ../Out_CustomMM_Peregrine;
