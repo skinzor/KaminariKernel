f10631d45a9759fa9d8f805b50e70b2122afc204
Kamin4ri <kamin4ridev@gmail.com>
2016-08-11 22:27:17 -0300

    build.sh: Add logic for both CM/AOSP and stock/IDCrisis versions
    Signed-off-by: Kamin4ri <kamin4ridev@gmail.com>

--- build.sh
+++ build.sh
@@ -14,17 +14,28 @@ export CROSS_COMPILE=arm-cortex_a7-linux-gnueabihf-;
 # Clear the screen, bud!
 clear;
 
+# Variables for bold & normal text
+bold=`tput bold`;
+normal=`tput sgr0`;
+
 # Let's start...
-echo -e "Building KaminariKernel (IDCrisis version)...\n";
+echo -e "Building KaminariKernel...\n";
 
 devicestr="Which device do you want to build for?
 1. Moto G (1st gen, GSM/CDMA) (falcon)
 2. Moto G (1st gen, LTE) (peregrine)
 3. Moto G (2nd gen, GSM/LTE) (titan/thea) ";
 
+romstr="Which ROM do you want to build this kernel for?
+1. Motorola Stock / Identity Crisis 6.0
+2. AOSP 6.0.x / CyanogenMod 13 and derivatives ";
+
 zipstr="Which installation type do you want to use?
 1. AnyKernel (recommended)
-2. Classic (boot.img) (Use if you have problems with AK) ";
+2. Classic (boot.img) (Use if you have problems with AK)
+
+${bold}Note:${normal} Classic mode is currently not available for AOSP/CM.
+If you choose it anyway, AnyKernel will be automatically selected. ";
 
 selstr="Do you want to force SELinux to stay in Permissive mode?
 Only say Yes if you're aware of the possible security risks this may introduce! (Y/N) ";
@@ -48,6 +59,23 @@ while read -p "$devicestr" dev; do
 			echo -e "\nInvalid option. Try again.\n";;
 	esac;
 done;
+
+# Select which ROM the kernel should be built for
+while read -p "$romstr" rom; do
+	case $rom in
+		"1")
+			echo -e "Selected ROM: Motorola Stock / IDCrisis 6.0\n"
+			rom="stock";
+			break;;
+		"2")
+			echo -e "Selected ROM: AOSP / CM13 & derivatives\n"
+			rom="cm";
+			break;;
+		*)
+			echo -e "\nInvalid option. Try again.\n";;
+	esac;
+done;		
+	
 		
 # Clean everything via `make mrproper`.
 # Recommended if there were extensive changes to the source code.
@@ -127,8 +155,13 @@ while read -p "$zipstr" zipmode; do
 			echo -e "Selected installation type: AnyKernel\n";
 			break;;
 		"2")
-			zipmode="classic";
-			echo -e "Selected installation type: Classic\n";			
+			if [[ $rom = "stock" ]]; then
+				zipmode="classic";
+				echo -e "Selected installation type: Classic\n";
+			else
+				zipmode="ak";
+				echo -e "Classic mode not yet available for CM. Using AnyKernel instead.\n";
+			fi;
 			break;;
 		*)
 			echo -e "\nInvalid option. Try again.\n";;
@@ -139,7 +172,7 @@ done;
 while read -p "$selstr" forceperm; do
 	case $forceperm in
 		"y" | "Y" | "yes" | "Yes")
-			echo -e "WARNING: SELinux will stay in Permissive mode at all times. You won't be able to change it to Enforcing.\nBe careful.\n";
+			echo -e "${bold}WARNING: SELinux will stay in Permissive mode at all times. You won't be able to change it to Enforcing.\nBe careful.\n${normal}";
 			forceperm="Y";
 			break;;
 		"n" | "N" | "no" | "No" | "" | " ")
@@ -159,7 +192,11 @@ if [[ $zipmode = "classic" ]]; then
 fi;
 			
 # Build the kernel
-make "$device"_defconfig;
+if [[ $rom = "stock" ]]; then
+	make "$device"_defconfig;
+else
+	make cm/"$device"_defconfig;
+fi;
 
 if [[ $jobs != "0" ]]; then
 	if [[ $forceperm = "Y" ]]; then
@@ -179,49 +216,70 @@ fi;
 echo -e "Build finished on:\n`date +"%A, %d %B %Y @ %H:%M:%S %Z (GMT %:z)"`\n`date --utc +"%A, %d %B %Y @ %H:%M:%S %Z"`\n";
 
 # Define directories (zip, out)
-if [[ $zipmode = "ak" ]]; then
-	maindir=$HOME/Kernel/Zip_CustomIdC_AK;
-	outdir=$HOME/Kernel/Out_CustomIdC_AK/$device;
+if [[ $rom = "stock" ]]; then
+	if [[ $zipmode = "ak" ]]; then
+		maindir=$HOME/Kernel/Zip_CustomIdC_AK;
+		outdir=$HOME/Kernel/Out_CustomIdC_AK/$device;
+	else
+		maindir=$HOME/Kernel/Zip_CustomIdC_BootImg;
+		outdir=$HOME/Kernel/Out_CustomIdC_BootImg/$device;
+	fi;
 else
-	maindir=$HOME/Kernel/Zip_CustomIdC_BootImg;
-	outdir=$HOME/Kernel/Out_CustomIdC_BootImg/$device;
+	if [[ $zipmode = "ak" ]]; then
+		maindir=$HOME/Kernel/Zip_CM_AK;
+		outdir=$HOME/Kernel/Out_CM_AK/$device;
+	else
+		maindir=$HOME/Kernel/Zip_CM_BootImg;
+		outdir=$HOME/Kernel/Out_CM_BootImg/$device;
+	fi;
 fi;
 devicedir=$maindir/$device;
+
 
 # Make the zip and out dirs if they don't exist
 if [ ! -d $maindir ] || [ ! -d $outdir ]; then
 	mkdir -p $maindir && mkdir -p $outdir;
 fi;
 
-# Make the modules dir if it doesn't exist.
-# Also remove any previously built modules
-if [[ $zipmode = "ak" ]]; then
-	[ -d $devicedir/modules ] || mkdir -p $devicedir/modules;
-	[ -d $devicedir/modules ] && rm -rf $devicedir/modules/*;
-	[ -d $devicedir/modules/pronto ] || mkdir -p $devicedir/modules/pronto;
-	moduledir=$devicedir/modules;
-else
-	[ -d $devicedir/system/lib/modules ] || mkdir -p $devicedir/system/lib/modules;
-	[ -d $devicedir/system/lib/modules ] && rm -rf $devicedir/system/lib/modules/*;	
-	[ -d $devicedir/system/lib/modules/pronto ] || mkdir -p $devicedir/system/lib/modules/pronto;
-	moduledir=$devicedir/system/lib/modules;
+# [For stock/IDCrisis ROM only] Make the modules dir if it doesn't exist.
+# Remove any previously built modules as well.
+if [[ $rom = "stock" ]]; then
+	if [[ $zipmode = "ak" ]]; then
+		[ -d $devicedir/modules ] || mkdir -p $devicedir/modules;
+		[ -d $devicedir/modules ] && rm -rf $devicedir/modules/*;
+		[ -d $devicedir/modules/pronto ] || mkdir -p $devicedir/modules/pronto;
+		moduledir=$devicedir/modules;
+	else
+		[ -d $devicedir/system/lib/modules ] || mkdir -p $devicedir/system/lib/modules;
+		[ -d $devicedir/system/lib/modules ] && rm -rf $devicedir/system/lib/modules/*;	
+		[ -d $devicedir/system/lib/modules/pronto ] || mkdir -p $devicedir/system/lib/modules/pronto;
+		moduledir=$devicedir/system/lib/modules;
+	fi;
+
+	# Copy the modules
+	echo -e "Copying kernel modules...\n";
+	for mod in `find . -type f -name "*.ko"`; do
+		cp -f $mod $moduledir/;
+	done;
+	# Move wi-fi module (wlan.ko) to modules/pronto & rename it to pronto_wlan.ko. This is very important!!
+	# A symlink to it (named wlan.ko) will be created at installation time.
+	mv $moduledir/wlan.ko $moduledir/pronto/pronto_wlan.ko;
 fi;
 
-# Copy the modules
-echo -e "Copying kernel modules...\n";
-for mod in `find . -type f -name "*.ko"`; do
-	cp -f $mod $moduledir/;
-done;
-# Move wi-fi module (wlan.ko) to modules/pronto & rename it to pronto_wlan.ko. This is very important!!
-# A symlink to it (named wlan.ko) will be created at installation time.
-mv $moduledir/wlan.ko $moduledir/pronto/pronto_wlan.ko;
+# Use zImage + dt.img instead of using zImage-dtb. This is what AnyKernel does by default.
+# This dt.img will also be used for classic mode.
+# The difference is, while AnyKernel does all its magic on the phone itself, classic mode will
+# do its job using the build machine (i.e. the computer), creating a boot.img from zImage, dt.img and a prepacked ramdisk.
+# In classic mode, we just need to dd the boot.img to the device-specific boot partition, thus using less resources.
+# AnyKernel, on the other hand, makes it easier to modify the kernel, especially the ramdisk.
+echo -e "Creating dt.img...\n"
+# Use dtbTool if building for the stock ROM; dtbToolCM if building for AOSP.
+if [[ $rom = "stock" ]]; then
+	./bootimgtools/dtbTool -s 2048 -o /tmp/dt.img -p scripts/dtc/ arch/arm/boot/;
+else
+	./bootimgtools/dtbToolCM -2 -s 2048 -o /tmp/dt.img -p scripts/dtc/ arch/arm/boot/;
+fi;
 
-# (Experimental - AnyKernel):
-# Use zImage and build a dt.img (the same way as the "classic" installation mode does).
-# The difference here is that we won't build a boot.img right now. Instead, the image will
-# be rebuilt by the phone itself, whilst the zip is being flashed.
-echo -e "Generating dt.img...\n"
-./bootimgtools/dtbTool -s 2048 -o /tmp/dt.img -p scripts/dtc/ arch/arm/boot/;
 # Only create a boot.img if we're using classic mode.
 if [[ $zipmode = "classic" ]]; then
 	echo -e "Creating boot.img...\n";
@@ -229,25 +287,29 @@ if [[ $zipmode = "classic" ]]; then
 	--kernel_offset 0x00008000 --ramdisk_offset 0x01000000 --tags_offset 0x00000100 \
 	--cmdline "console=ttyHSL0,115200,n8 androidboot.console=ttyHSL0 androidboot.hardware=qcom user_debug=31 msm_rtb.filter=0x37 vmalloc=400M utags.blkdev=/dev/block/platform/msm_sdcc.1/by-name/utags movablecore=160M" \
 	--pagesize 2048 --dt /tmp/dt.img --output $devicedir/boot.img;
-else
+else # Just copy zImage and dt.img. AnyKernel will do the rest later.
 	echo -e "Copying zImage & dt.img...\n";
 	cp -f /tmp/dt.img $devicedir/;
 	cp -f arch/arm/boot/zImage $devicedir/;
 fi;
 
-# (Disabled) Copy zImage-dtb
-# cp -f arch/arm/boot/zImage-dtb ../Zip_CustomMM_$name/;
-# ls -l ../Zip_CustomMM_$name/zImage-dtb;
-# cd ../Zip_CustomMM_$name;
-
-# Set zip name
-if [[ $forceperm = "Y" ]]; then
-	zipname="Kaminari_"$version"_"`echo "${device^}"`"_Permissive";
+# Set the zip's name
+if [[ $rom = "stock" ]]; then
+	if [[ $forceperm = "Y" ]]; then
+		zipname="Kaminari_"$version"_"`echo "${device^}"`"_Permissive";
+	else
+		zipname="Kaminari_"$version"_"`echo "${device^}"`;
+	fi;
 else
-	zipname="Kaminari_"$version"_"`echo "${device^}"`;
+	if [[ $forceperm = "Y" ]]; then
+		zipname="KaminariCM_"$version"_"`echo "${device^}"`"_Permissive";
+	else
+		zipname="KaminariCM_"$version"_"`echo "${device^}"`;
+	fi;
 fi;
 
-# Make the zip & finish
+# Zip the stuff we need & finish
+echo -e "Creating flashable ZIP...\n";
 case $device in
 	"falcon")
 		echo -e "Device: Moto G 1st Gen (falcon)" > $devicedir/device.txt;;
